11944b063541ab819cb2ee652b0408db39f3d6af
Anderson Gomes <54241354+Nosredzzz@users.noreply.github.com>
2024-08-22 18:41:25 -0300

    Update app_setup.py

--- app_setup.py
+++ app_setup.py
@@ -28,26 +28,26 @@ def main():
     # Get the current script directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # Construct path to sibling directory
-    script_dir = os.path.join(current_dir, "..", "aws-common")
+    common_project_dir = os.path.join(current_dir, "..", "aws-common")
 
     # check if script directory exists
-    if not os.path.exists(script_dir):
-        print(f"Script directory not found: {script_dir}")
+    if not os.path.exists(common_project_dir):
+        print(f"Script directory not found: {common_project_dir}")
         # call the git clone command
         parent_dir = os.path.join(current_dir, "..")
         _run_command(
             f"git clone https://github.com/LARS-Org/aws-common.git",
             cwd=parent_dir,
             shell=True,
-        )   
-        print(f"Cloned the aws-common repository to {script_dir}")
+        )
+        print(f"Cloned the aws-common repository to {common_project_dir}")
     # script directory exists, update it
     # call the git pull command to ensure the latest version
-    _run_command("git fetch --quiet", cwd=script_dir, shell=True)
-    _run_command("git pull --quiet", cwd=script_dir, shell=True)
+    _run_command("git fetch --quiet", cwd=common_project_dir, shell=True)
+    _run_command("git pull --quiet", cwd=common_project_dir, shell=True)
 
     # calling the execute script passing the sys.argv
-    script_path = os.path.join(script_dir, "app_scripts")
+    script_path = os.path.join(common_project_dir, "app_scripts")
     script_path = os.path.join(script_path, "app_setup_execution.py")
     # convert the sys.argv to a string
     sys_argv = " ".join(sys.argv[1:])  # remove the first argument.
